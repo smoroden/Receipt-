eccbd0853ee02d9a9e93d5229a7f098f25a453a3
Zach Smoroden <zach@smoroden.com>
2016-05-26 11:07:06 -0700

    initial project setup

---
+++ Receipts++/Receipts++/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Receipts++
+//
+//  Created by Zach Smoroden on 2016-05-26.
+//  Copyright © 2016 Zach Smoroden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Receipts++/Receipts++/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Receipts++
+//
+//  Created by Zach Smoroden on 2016-05-26.
+//  Copyright © 2016 Zach Smoroden. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
